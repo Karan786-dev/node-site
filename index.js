731bb03afeb9dd928cb9c1044258f84cd442a549
Karan786-dev <81469693+Karan786-dev@users.noreply.github.com>
2022-08-25 15:03:00 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 const express = require("express");
+const axios = require('axios')
 var  app = express()
 const path = require('path')
+const api_url = 'https://user.devlopath.com/instant/fetch-camp-info.php?token=eeae5f78-a2b1-11ec-958c-7c10c91d52d7'
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/',(req,res)=>{
-  const data = [
-    {
-    name : 'Karan',
-    },
-    {
-      name : 'Arsh'
-    }
-  ]
+app.get('/',async (req,res)=>{
+  let data = await axios.get(api_url)
   res.render('home',{alldata : data})
 })
 
