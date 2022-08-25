bb1f5594b9e7db7ca939794019f74a0f53c5de37
Karan786-dev <81469693+Karan786-dev@users.noreply.github.com>
2022-08-25 14:57:51 +0530

    update index.js

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/',(req,res)=>{
-  const data = {
+  const data = [{
     name : 'Karan',
     name : 'Arsh'
-  }
+  }]
   res.render('home',{alldata : data})
 })
 
