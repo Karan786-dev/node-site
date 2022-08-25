0b8fdeb368d2ba812f7f4a3c06e51d7319b16ef3
Karan786-dev <81469693+Karan786-dev@users.noreply.github.com>
2022-08-25 06:11:33 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+const express = require("express");
+var  app = express()
 
+app.set('view engine', 'ejs');
+
+app.get('/',(req,res)=>{
+  res.render('index',{name : 'Karan'})
+})
+
+app.listen(8080)
